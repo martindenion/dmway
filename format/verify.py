0b4fdd2076341152fa614cbaef6d6275a238723d
martindenion <54849945+martindenion@users.noreply.github.com>
2019-10-09 14:49:31 +0200

    Initial commit

---
+++ format/verify.py
@@ -0,0 +1,60 @@
+import json
+from functools import reduce
+
+
+class Verification:
+    """Cette classe consiste en la vérification du format JSON
+    des données reçues par les différents capteurs"""
+
+    def __init__(self):
+        """Par défaut, la vérification du format est un échec"""
+        self.success_keys = False
+        self.success_values = False
+
+    def json_to_dict(self, raw_json):
+        """Méthode qui permet de convertir un message JSON en dictionnaire python"""
+        return json.loads(raw_json)
+
+    def _get_ts(self, raw_json):
+        raw_dict = self.json_to_dict(raw_json)
+        return raw_dict["ts"]
+
+    def _get_keys(self, raw_json):
+        """Méthode qui retourne une liste des clés de la trame JSON"""
+        raw_dict = self.json_to_dict(raw_json)
+        keys_list = []
+        for cle in raw_dict.keys():
+            keys_list.append(cle)
+        return keys_list
+
+    def _get_values(self, raw_json):
+        """Méthode qui retourne une liste des valeurs de la trame JSON"""
+        raw_dict = self.json_to_dict(raw_json)
+        values_list = []
+        for cle in raw_dict.values():
+            values_list.append(cle)
+        return values_list
+
+    def verify_keys(self, raw_json):
+        """"Méthode qui vérifie la validité des keys de la trame JSON"""
+        keys_list = self._get_keys(raw_json)
+        model_keys_list = ['ts', 'temperature', 'humidity', 'pressure', 'luminosity', 'sound']
+        for key in keys_list:
+            print('key : {}'.format(key))
+            if key in model_keys_list:
+                self.success_keys = True
+            else:
+                self.success_keys = False
+                break
+        return self.success_keys
+
+    def verify_values(self, raw_json):
+        """Méthode qui vérifie la validité des values de la trame JSON"""
+        values_list = self._get_values(raw_json)
+        for value in values_list:
+            if isinstance(value, int) or isinstance(value, float):
+                self.success_values = True
+            else:
+                self.success_values = False
+                break
+        return self.success_values
---
+++ listener/listener.py
@@ -0,0 +1,59 @@
+
+#!/usr/bin/env python
+import os
+import time
+import serial
+import sys
+import paho.mqtt.client as mqtt
+import json
+
+THINGSBOARD_HOST = 'iotplatform.int.cetic.be'
+ACCESS_TOKEN = 'P1Wx6MO4SAgAr3nnqqYV'
+
+# Data capture and upload interval in seconds. Less interval will eventually ha$
+INTERVAL=10
+
+sensor_data = {'temperature': 0, 'humidity': 0}
+
+next_reading = time.time()
+
+client = mqtt.Client()
+
+# Set access token
+client.username_pw_set(ACCESS_TOKEN)
+
+# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive inter$
+client.connect(THINGSBOARD_HOST, 1883, 60)
+
+client.loop_start()
+
+ser = serial.Serial(
+ port='/dev/ttyUSB0',
+ baudrate = 115200,
+ parity=serial.PARITY_NONE,
+ stopbits=serial.STOPBITS_ONE,
+ bytesize=serial.EIGHTBITS,
+ timeout=1
+)
+counter=0
+
+try:
+    while True:
+        luminosity = ser.readline()
+        # print(u"Temperature: {:g}\u00b0C, Humidity: {:g}%".format(temperature,$
+        # sensor_data['temperature'] = temperature
+        # sensor_data['humidity'] = humidity
+
+        # Sending humidity and temperature data to ThingsBoard
+        client.publish('v1/devices/me/telemetry', luminosity)
+
+        next_reading += INTERVAL
+sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect()
+
