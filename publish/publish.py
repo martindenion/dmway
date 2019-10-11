66399cefb0aa363ab28029cd271f51447dc4c169
martindenion <54849945+martindenion@users.noreply.github.com>
2019-10-11 16:12:15 +0200

    Delete function completed

--- publish/publish.py
+++ publish/publish.py
@@ -1,21 +1,21 @@
 
 #!/usr/bin/env python
-import os
-import time
-import serial
-import sys
+#import os
+#import time
+#import serial
+#import sys
 import paho.mqtt.client as mqtt
 import json
 #send data to thingsboard
 THINGSBOARD_HOST = 'iotplatform.int.cetic.be'
-ACCESS_TOKEN = 'P1Wx6MO4SAgAr3nnqqYV'
+ACCESS_TOKEN = 'cmrtWotOUSysmsydspo4'
 
 # Data capture and upload interval in seconds. Less interval will eventually ha$
 INTERVAL=10
+device = '{"device": "deviceZ"}'
+#sensor_data = {'temperature': 0, 'humidity': 0}
 
-sensor_data = {'temperature': 0, 'humidity': 0}
-
-next_reading = time.time()
+#next_reading = time.time()
 
 client = mqtt.Client()
 
@@ -26,7 +26,7 @@ client.username_pw_set(ACCESS_TOKEN)
 client.connect(THINGSBOARD_HOST, 1883, 60)
 
 client.loop_start()
-
+"""
 ser = serial.Serial(
  port='/dev/ttyUSB0',
  baudrate = 115200,
@@ -39,20 +39,22 @@ counter=0
 
 try:
     while True:
-        luminosity = ser.readline()
+        #luminosity = ser.readline()
         # print(u"Temperature: {:g}\u00b0C, Humidity: {:g}%".format(temperature,$
         # sensor_data['temperature'] = temperature
         # sensor_data['humidity'] = humidity
 
         # Sending humidity and temperature data to ThingsBoard
-        client.publish('v1/devices/me/telemetry', luminosity)
-
+        #client.publish('v1/devices/me/telemetry', luminosity)
+        
         next_reading += INTERVAL
 sleep_time = next_reading-time.time()
         if sleep_time > 0:
             time.sleep(sleep_time)
 except KeyboardInterrupt:
     pass
+"""
+client.publish('v1/gateway/connect', device)
 
 client.loop_stop()
 client.disconnect()
