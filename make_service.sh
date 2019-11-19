99cfd3a4b03d20804c67b811e25b506b4f96bae4
martindenion <54849945+martindenion@users.noreply.github.com>
2019-11-19 11:57:04 +0100

    Create make_service.sh

---
+++ make_service.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]
+then
+echo "dmway service is enabled"
+exit 1
+fi
+
+NAME=$1
+SCRIPT_FILE=$2
+AFTER=$3
+REMAIN_AFTER_EXIT=$4
+
+SERVICE_FILE="/etc/systemd/system/$1.service"
+echo "[Unit]" >$SERVICE_FILE
+echo "Description=Service basique pour lancer $SCRIPT_FILE" >>$SERVICE_FILE
+echo "After=$AFTER" >>$SERVICE_FILE
+echo "Requires=$AFTER" >>$SERVICE_FILE
+echo "[Service]"  >>$SERVICE_FILE
+if [ "$REMAIN_AFTER_EXIT" == "no" ]
+then echo "Type=simple" >>$SERVICE_FILE;
+else echo "Type=forking" >>$SERVICE_FILE; fi
+echo "ExecStart=$SCRIPT_FILE" >>$SERVICE_FILE
+echo "TimeoutSec=0" >>$SERVICE_FILE
+echo "RemainAfterExit=$REMAIN_AFTER_EXIT" >>$SERVICE_FILE
+echo "SysVStartPriority=99" >>$SERVICE_FILE
+echo "RestartSec=5" >>$SERVICE_FILE
+echo "Restart=on-failure" >>$SERVICE_FILE
+echo "[Install]" >>$SERVICE_FILE
+echo "WantedBy=multi-user.target" >>$SERVICE_FILE
+chmod 755 $SCRIPT_FILE
+systemctl -q enable $NAME
