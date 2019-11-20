13c8a5c04b96718ecdf9d7c721d1fb14340a4a50
martindenion <54849945+martindenion@users.noreply.github.com>
2019-11-20 16:41:27 +0100

    Some bugs fixed

--- test/format/test_verification.py
+++ test/format/test_verification.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-class TestVerification(TestCase):
+class TestVerificationZwave(TestCase):
 
     def test_init(self):
         raw_json_empty = {"device": "", "type": "", "values": {}}
@@ -16,3 +16,38 @@ class TestVerification(TestCase):
         verif.filter_data("zwave/+/energy", raw_json)
         self.assertEqual(verif.filtered_dict, raw_dict_filtered)
 
+"""Test about JSON format"""
+
+    def test_zwave_wrong_topic(self):
+        raw_json = '{"node_id":"zwave","label":"fibaro","energy":30,"power":30}'
+        raw_dict_filtered = {'device': '', 'type': '', 'values': {}}
+        from src.format.format import Verification
+        verif = Verification()
+        verif.filter_data("zwav/+/energy", raw_json)
+        self.assertEqual(verif.filtered_dict, raw_dict_filtered)
+
+"""Tests about type of the value corresponding to each keys in the dictionary"""
+
+    def test_zwave_wrong_device_value_type(self):
+        raw_json = '{"node_id":zwave,"label":"fibaro","energy":30,"power":30}'
+        raw_dict_filtered = {'device': '', 'type': '', 'values': {}}
+        from src.format.format import Verification
+        verif = Verification()
+        verif.filter_data("zwave/+/energy", raw_json)
+        self.assertEqual(verif.filtered_dict, raw_dict_filtered)
+
+    def test_zwave_wrong_type_value_type(self):
+        raw_json = '{"node_id":"zwave","label":fibaro,"energy":30,"power":30}'
+        raw_dict_filtered = {'device': '', 'type': '', 'values': {}}
+        from src.format.format import Verification
+        verif = Verification()
+        verif.filter_data("zwave/+/energy", raw_json)
+        self.assertEqual(verif.filtered_dict, raw_dict_filtered)
+
+    def test_zwave_wrong_value_value_type(self):
+        raw_json = '{"node_id":"zwave","label":"fibaro","energy":"30","power":30}'
+        raw_dict_filtered = {'device': 'zwave', 'type': 'fibaro', 'values': {'power': 30}}
+        from src.format.format import Verification
+        verif = Verification()
+        verif.filter_data("zwave/+/energy", raw_json)
+        self.assertEqual(verif.filtered_dict, raw_dict_filtered)
