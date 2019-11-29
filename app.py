929f9ad301a3a405fff4f813d33cc87f3aaa5479
martindenion <54849945+martindenion@users.noreply.github.com>
2019-11-29 09:32:50 +0100

    Cleaning process method updated

--- app.py
+++ app.py
@@ -8,25 +8,26 @@ import logging
 from src.path import Path
 from src.subscribe.manag_sub_thread import ManagSubThread
 
-sub = None
+m = None
 
 
 def running_handler(signum, frame):
-    global sub
+    global m
     try:
         print("Cleaning process")
-        sub.stop_running()
-        sub.join()
+        m.stop_running()
+        m.join()
+
     except:
         pass
     sys.exit(0)
 
 
 def main_app():
-    global sub
     signal.signal(signal.SIGINT, running_handler)
     p = Path(['broker.json', 'schema.json'])
     p.search_paths()
+    global m
     m = ManagSubThread(p)
     m.start()
     logging.basicConfig(filename="dmway.log", level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s', datefmt='%d-%b-%y %H:%M:%S')
